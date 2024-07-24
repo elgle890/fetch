2689b95b0d95ae44c8c52ebb3a0254a27bfdf62d
Arthur <arthurpecegueiro2@gmail.com>
2024-07-24 21:14:26 +1000

    added files

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nome = document.querySelector('.profile-name');
+    const user = document.querySelector('.profile-username');
+    const avatar = document.querySelector('.profile-avatar');
+    const rep = document.querySelector('#rep');
+    const seguidores = document.querySelector('#seguidores');
+    const seguindo = document.querySelector('#seguindo');
+    const endpoint = 'https://api.github.com/users/ogiansouza';
+    const url = document.querySelector('#url');
+
+   // const xhttp = new XMLHttpRequest();
+   // xhttp.open('GET', endpoint);
+   // xhttp.send();
+    
+    
+    fetch(endpoint).then(function(resposta) {
+        return resposta.json();
+    }).then(function(json) {
+        nome.innerText = json.name;
+        user.innerText = json.login;
+        avatar.src =  json.avatar_url;
+        rep.innerText =  json.public_repos;
+        seguidores.innerText =  json.followers;
+        seguindo.innerText = json.following;
+        url.href =  json.html_url;
+    });
+
+});
